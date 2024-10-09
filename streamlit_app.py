31ad6a472c5164cccc45bfaed49e5af09a873b75
Ankit Kumar <akkiankitilu@gmail.com>
2024-10-09 10:10:16 +0530

    Update streamlit_app.py
    Fixing issue related to fruit_choosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingredient_list:
     ingredients_string = ""
     for fruit_choosen in ingredient_list:
         ingredients_string += fruit_choosen + " "
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
         st.subheader(fruit_choosen + "Nutrition Information")
         fruityvice_response = requests.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + fruit_choosen)
         fv_df = st.dataframe(fruityvice_response.json(), use_container_width=True)
